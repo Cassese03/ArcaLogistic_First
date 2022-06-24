2885f1cfe4b1192a632b293b2cbd284fa610e344
Cassese03 <casseselorenzo03@gmail.com>
2022-06-24 15:21:03 +0200

    Prevent Scroll on Focus

--- resources/views/carico_magazzino_pks.blade.php
+++ resources/views/carico_magazzino_pks.blade.php
@@ -19,6 +19,56 @@
 <style>
     @charset "UTF-8";
 
+    .dot-flashing {
+        position: relative;
+        width: 10px;
+        height: 10px;
+        border-radius: 5px;
+        background-color: #9880ff;
+        color: #9880ff;
+        animation: dotFlashing 1s infinite linear alternate;
+        animation-delay: .5s;
+    }
+
+    .dot-flashing::before, .dot-flashing::after {
+        content: '';
+        display: inline-block;
+        position: absolute;
+        top: 0;
+    }
+
+    .dot-flashing::before {
+        left: -15px;
+        width: 10px;
+        height: 10px;
+        border-radius: 5px;
+        background-color: #9880ff;
+        color: #9880ff;
+        animation: dotFlashing 1s infinite alternate;
+        animation-delay: 0s;
+    }
+
+    .dot-flashing::after {
+        left: 15px;
+        width: 10px;
+        height: 10px;
+        border-radius: 5px;
+        background-color: #9880ff;
+        color: #9880ff;
+        animation: dotFlashing 1s infinite alternate;
+        animation-delay: 1s;
+    }
+
+    @keyframes dotFlashing {
+        0% {
+            background-color: #9880ff;
+        }
+        50%,
+        100% {
+            background-color: #ebe6ff;
+        }
+    }
+
     .divider {
         border-right: 1px solid black;
         padding: 0 0 0 0;
@@ -1382,6 +1432,7 @@
                     },
                     error: function (result) {
                         $('#ajax_loader').fadeOut();
+                        alert('Errore! Documento non evaso');
                         location.reload();
                     }
                 })
@@ -1463,6 +1514,7 @@
                     },
                     error: function (result) {
                         $('#ajax_loader').fadeOut();
+                        alert('Errore! Documento non evaso');
                         location.reload();
                     }
                 })
--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,7 +1,7 @@
 <script src="/js/jquery-3.2.1.min.js"></script>
 <div style="position: fixed;width: 100%;height: 100%;background: rgba(255, 255, 255,1);z-index: 1000000000;display: none;" id="ajax_loader">
     <img src="<?php echo URL::asset('img/logo_arca.png') ?>" alt="AdminLTE Logo" style="width:200px;margin:0 auto;display:block;margin-top:200px;">
-    <h4 style="text-align:center;margin-top:10px;">Operazione In Corso....</h4>
+    <h4 style="text-align:center;margin-top:10px;">Operazione In Corso <div class="dot-flashing" style="margin:0 auto;display:block;color: black!important;"></div> </h4>
 </div>
 
 
