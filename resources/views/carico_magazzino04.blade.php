64c8381daf56e0391e506d73fa557a5d4b316ce2
Cassese03 <casseselorenzo03@gmail.com>
2022-09-23 11:57:28 +0200

    Scanner detection

--- resources/views/carico_magazzino04.blade.php
+++ resources/views/carico_magazzino04.blade.php
@@ -714,12 +714,14 @@
     $(document).scannerDetection({
         timeBeforeScanTest: 200, // wait for the next character for upto 200ms
         startChar: [120], // Prefix character for the cabled scanner (OPL6845R)
-        endChar: [13], // be sure the scan is complete if key 13 (enter) is detec
+        endChar: [9], // be sure the scan is complete if key 13 (enter) is detec
         // ted
         avgTimeByChar: 40, // it's not a barcode if a character takes longer than 40ms
         onComplete: function(code, qty){
-            document.getElementById('cerca_articolo2').value = code;
-            check();
+            if(document.activeElement.classList.contains('form-control')== false){
+                document.getElementById('cerca_articolo2').value = code;
+                check();
+            }
         } // main callback function
 
     });
